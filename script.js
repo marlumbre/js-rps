d54617dd1e729e69580ccb4667198c80d3835020
marlumbre <101400268+marlumbre@users.noreply.github.com>
2025-04-10 22:58:46 +0800

    Change output text to be more concise

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
 function getHumanChoice() {
-    // same RNG from getComputerChoice() just to make testing easier
-    /*
-    randomChoice = Math.random();
-    console.log(randomChoice);
-    if (randomChoice <= 0.3) {
-        return "rock"; 
-    } 
-    else if (randomChoice <= 0.6) {
-        return "paper";
-    }
-    else {
-        return "scissors";
-    }
-        */
-
-    // gets player choice
-    
     while(true) {
         playerChoice = prompt("Rock, Paper, or Scissors?");
         playerChoice = playerChoice.toLowerCase();
@@ -27,7 +10,7 @@ function getHumanChoice() {
             case "scissors" :
                 return "scissors";
             default:
-                console.log("Invalid play, try again");
+                alert("Invalid play, try again");
         }
     }
 }
@@ -48,40 +31,48 @@ function getComputerChoice() {
 
 function playRound(humanChoice, computerChoice) {
 
+    choicesMade = humanChoice + computerChoice;
+
     const playerWin = "The player wins.";
     const compWin = "The computer wins.";
 
     // Output plays
-    console.log("The player played " + humanChoice + "!");
-    console.log("The computer played " + computerChoice + "!");
+    console.log("Player: " + humanChoice);
+    console.log("Computer: " + computerChoice);
     
     // Check for winner or draw
-    switch (humanChoice, computerChoice) {
-        case (humanChoice === "rock" && computerChoice === "scissors") :
-            humanScore += 1;
+    switch (choicesMade) {
+        case ("rockscissors") :
+            humanScore++;
             console.log(playerWin);
-        case (humanChoice === "rock" && computerChoice === "paper") :
-            computerScore += 1;
+            break;
+        case ("rockpaper") :
+            computerScore++;
             console.log(compWin);
-        case (humanChoice === "paper" && computerChoice ==="rock") :
-            humanScore += 1;
+            break;
+        case ("paperrock") :
+            humanScore++;
             console.log(playerWin);
-        case (humanChoice === "paper" && computerChoice === "scissors") :
-            computerScore += 1;
+            break;
+        case ("paperscissors") :
+            computerScore++;
             console.log(compWin);
-        case (humanChoice === "scissors" && computerChoice === "paper") :
-            humanScore += 1;
+            break;
+        case ("scissorspaper") :
+            humanScore++;
             console.log(playerWin);
-        case (humanChoice === "scissors" && computerChoice === "rock") :
-            computerScore += 1;
+            break;
+        case ("scissorsrock") :
+            computerScore++;
             console.log(compWin);
+            break;
         default :
             console.log("It's a draw!");
+            break;
     }
 
     // Output scores
-    console.log("The player's score is: " + humanScore);
-    console.log("The computer's score is: " + computerScore);
+    console.log("Player: " + humanScore + ", Computer: " + computerScore);
 }
 
 let humanScore = 0;
@@ -90,3 +81,7 @@ const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
 playRound(humanSelection, computerSelection);
+playRound(getHumanChoice(), getComputerChoice());
+playRound(getHumanChoice(), getComputerChoice());
+playRound(getHumanChoice(), getComputerChoice());
+playRound(getHumanChoice(), getComputerChoice());
